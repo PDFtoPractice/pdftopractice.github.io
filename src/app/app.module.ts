d2bfde3bdcd261198c9ff6d2e2f23f872fda9e3b
Adam Smith <adamcsmith98@gmail.com>
2018-02-06 15:57:32 +0000

    Initial data card (input and result)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NameCardComponent } from './name-card/name-card.component';
 import { FilterCardComponent } from './filter-card/filter-card.component';
 import { DescriptionCardComponent } from './description-card/description-card.component';
+import { DataCardComponent } from './data-card/data-card.component';
+
+import { AdviceService } from './advice.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameCardComponent,
     FilterCardComponent,
-    DescriptionCardComponent
+    DescriptionCardComponent,
+    DataCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialImportsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AdviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
