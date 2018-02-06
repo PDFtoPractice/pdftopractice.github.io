7e375228f7255528fcf7861b2fd841ad2b9f8f27
roxdumitriu <rox.dumitriu@gmail.com>
2018-02-06 13:34:35 +0000

    Temporary first page.

--- pdftopractice/src/app/description-card/description-card.component.ts
+++ pdftopractice/src/app/description-card/description-card.component.ts
@@ -12,4 +12,21 @@ export class DescriptionCardComponent implements OnInit {
   ngOnInit() {
   }
 
+  openDescription(evt, tabTitle) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    if (tabTitle == "defaultOpen") {
+      document.getElementById(tabTitle).click();
+    }
+    document.getElementById(tabTitle).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
 }
