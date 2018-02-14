fe23be1a063d15500b147d9320c2faf032c12253
Adam Smith <adamcsmith98@gmail.com>
2018-02-14 17:43:22 +0000

    Add test for service

--- src/app/advice.service.spec.ts
+++ src/app/advice.service.spec.ts
@@ -3,6 +3,9 @@ import { TestBed, inject } from '@angular/core/testing';
 import { AdviceService } from './advice.service';
 import { MockAdviceService } from './mock-advice.service';
 
+import { Observable } from 'rxjs/Observable';
+import { Advice } from './model/Advice';
+
 let mockService;
 describe('AdviceService', () => {
   beforeEach(() => {
@@ -14,5 +17,13 @@ describe('AdviceService', () => {
 
   it('should be created', () => {
     expect(mockService).toBeTruthy();
+  });
+
+  it('should return advice observable', () => {
+    mockService.getAdvice('DrugA', 'OperationA').subscribe((advice : Advice) => {
+      expect(advice.drug).toBe('DrugA');
+      expect(advice.operation).toBe('OperationA');
+      expect(advice.advice).toBe('Consult a doctor');
+    });
   });
 });
