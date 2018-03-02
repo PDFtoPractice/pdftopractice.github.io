eba11e0f235b0f489e205870f55b559b16391dc1
Adam Smith <adamcsmith98@gmail.com>
2018-03-02 14:39:52 +0000

    Add ignored links file

---
+++ src/app/links.ts
@@ -0,0 +1,5 @@
+export enum Links {
+  DrugCondition = 'https://yzs63ht9tg.execute-api.eu-west-1.amazonaws.com/prod/DrugCondition',
+  DrugInteraction = 'https://yzs63ht9tg.execute-api.eu-west-1.amazonaws.com/prod/DrugInteraction',
+  DrugPerioperative = 'https://yzs63ht9tg.execute-api.eu-west-1.amazonaws.com/prod/DrugPerioperative'
+}
